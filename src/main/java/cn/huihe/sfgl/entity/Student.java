22e0e3038404d0614c94a30999e97c6213234949
dell <dell@windows10.microdone.cn>
2017-11-21 21:07:14 +0800

    tianyu Students entity

---
+++ src/main/java/cn/huihe/sfgl/entity/Student.java
@@ -0,0 +1,25 @@
+package cn.huihe.sfgl.entity;
+
+import java.util.List;
+
+import cn.huihe.sfgl.service.SignService;
+
+public class Student implements SignService{
+	
+	private Integer sid;
+	private String sname;
+	private Double stotal;
+	private String sfee;
+	
+	public List<Student> SearchForStudents(Integer gid, Integer cid) {
+		// 	 根据年级(gid)和班级(cid)查询出对应某年级某班所有学生的信息
+		return null;
+	}
+	public Boolean AchieveSign(Student stu) {
+		// 实现签到,返回false或true来表示签到是否成功
+		return null;
+	}
+	public static void main(String[] args) {
+		System.out.println(" ");
+	}
+}
