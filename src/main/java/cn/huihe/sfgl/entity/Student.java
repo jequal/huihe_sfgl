2516091008690d8dfd25be98278e16dfc2740b1f
dell <dell@windows10.microdone.cn>
2017-11-21 21:24:35 +0800

    Bailing student entity

--- src/main/java/cn/huihe/sfgl/entity/Student.java
+++ src/main/java/cn/huihe/sfgl/entity/Student.java
@@ -9,7 +9,8 @@ public class Student implements SignService{
 	private Integer sid;
 	private String sname;
 	private Double stotal;
-	private String sfee;
+	private Integer sfee;
+	
 	
 	public List<Student> SearchForStudents(Integer gid, Integer cid) {
 		// 	 根据年级(gid)和班级(cid)查询出对应某年级某班所有学生的信息
